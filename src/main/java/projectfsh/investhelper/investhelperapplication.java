b715b60cc1a68896af866215ae89816b59c38c75
Guilherme Ferreira <gferreiradasilva96@gmail.com>
2024-10-22 14:30:41 -0300

    First commit

---
+++ src/main/java/projectfsh/investhelper/investhelperapplication.java
@@ -0,0 +1,13 @@
+package projectfsh.investhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class investhelperapplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(investhelperapplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectfsh/investhelper/InvesthelperApplicationTests.java
@@ -0,0 +1,13 @@
+package projectfsh.investhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvesthelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
